24d711a588afee18063815482666a20efb250372
Arihant Jain <suhailarihant@gmail.com>
2025-10-26 18:53:12 +0800

    fix-glbc issue

--- startup.sh
+++ startup.sh
@@ -8,6 +8,7 @@ export PYTHONPATH="/home/site/wwwroot:${PYTHONPATH:-}"
 python -V || true
 python -c "import sys, pkgutil; print('PYTHONPATH=', sys.path)" || true
 python -c "import uvicorn, gunicorn; print('uvicorn', uvicorn.__version__)" || true
+python -c "import sys, cryptography; print('cryptography=', cryptography.__version__, cryptography.__file__); print('sys.maxunicode=', sys.maxunicode)" || true
 
 # Start the API
 exec gunicorn -w 1 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:${PORT}
